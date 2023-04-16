48f9bcae574f0cccc54a1a6205e3a6e393a24c35
Arie3s <104190110+Arie3s@users.noreply.github.com>
2023-04-17 01:41:17 +0500

    final touches
    font,color,align,sizing

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ fetch('data.json')
                 <th>Rank</th>
                 <th></th>
                 <th>Title</th>
-                <th>Genre</th>
+                <th>Synopsis</th>
                 <th>Year</th>
                 <th>Language</th>
                 <th>Rating</th>
@@ -25,8 +25,8 @@ fetch('data.json')
             <tr>
                 <td>${i}</td>
                 <td ><img class="px-2 inline-block" src="${'https://image.tmdb.org/t/p/w45' + movie.poster_path}" />
-                <td>${movie.title}</td>
-                <td>${movie.genres.toString(", ")}</td>
+                <td class="px-6 py-3 text-left">${movie.title}<br><span style="color: #888888;  font-size: 0.8rem">${movie.genres.toString(", ")} . ${movie.runtime} min</span></td>
+                <td class="px-6 py-3 text-left" style="font-size: 0.9rem;">${movie.overview}</td>
                 <td>${movie.release_date.substring(0,4)}</td>
                 <td>${movie.original_language}</td>
                 <td>${movie.vote_average}</td>
@@ -46,7 +46,7 @@ fetch('data.json')
 
     // Filter movies based on selected options and update table
     function filter() {
-        let genre = document.getElementById("genre").value.toLowerCase();
+        let genre = document.getElementById("genre").value.toLowerCase().split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
         let year = document.getElementById("year").value;
         let language = document.getElementById("language").value.toLowerCase();
         let rating = document.getElementById("rating").value;
@@ -65,9 +65,10 @@ fetch('data.json')
         out = `
             <thead>
                 <tr>
+                    <th>Rank</th>
                     <th></th>
                     <th>Title</th>
-                    <th>Genre</th>
+                    <th>Synopsis</th>
                     <th>Year</th>
                     <th>Language</th>
                     <th>Rating</th>
@@ -83,13 +84,14 @@ fetch('data.json')
                 <tr>
                     <td>${i}</td>
                     <td><img class="px-2 inline-block" src="${'https://image.tmdb.org/t/p/w45' + movie.poster_path}" />
-                    <td>${movie.title}</td>
-                    <td>${movie.genres.toString(", ")}</td>
+                    <td class="px-6 py-3 text-left">${movie.title}<br><span style="color: #888888;  font-size: 0.8rem">${movie.genres.toString(", ")} . ${movie.runtime} min</span></td>
+                    <td class="px-6 py-3 text-left" style="font-size: 0.9rem;">${movie.overview}</td>
                     <td>${movie.release_date.substring(0,4)}</td>
                     <td>${movie.original_language}</td>
                     <td>${movie.vote_average}</td>
                 </tr>
             `;
+            i++;
         }
         
         out += `</tbody>`;
